98aea9a9fc6c764949daa90bdb7f07a1489f4ea4
SantanuAdhikary <santanuadhikary13@gmail.com>
2026-02-11 20:11:08 +0530

    insertion sort

---
+++ Array/InsertionSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+class InsertionSort 
+{
+	public static void main(String[] args) 
+	{
+		int arr[] = {5,4,3,2,1};
+		System.out.println("before sorting");
+		System.out.println(Arrays.toString(arr));
+		for(int i=1 ; i<arr.length;i++)
+		{
+			int key = arr[i] ;
+			int j = i- 1;	
+			while( j>=0 && arr[j] > key  )
+			{
+				  arr[j+1] =arr[j];
+			      j--;	  
+			}	
+			arr[j+1] = key;
+		}
+		System.out.println("after sorting");
+		System.out.println(Arrays.toString(arr));
+	}
+}
